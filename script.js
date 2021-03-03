dd759889c25d971f2dda22c15ffdd729c0403ae9
jthinlay <jthinlay@hotmail.com>
2021-03-03 14:18:58 -0800

    Initial Commit of the Project

--- script.js
+++ script.js
@@ -26,31 +26,31 @@ function showContent(page){
 
 function createDOMNodes(page){
     const currentArray = page === 'results' ? resultsArray : Object.values(favorites); 
-    currentArray.forEach((result) => {
-        // Card Container
-        const card = document.createElement('div');
-              card.classList.add('card');
-        // Link 
-        const link = document.createElement('a');
-              link.href = result.dhUrl;
-              link.title = 'View Full Image';
-              link.target = '_blank';
-        // Image
-        const image = document.createElement('img');
-              image.src = result.url;
-              image.alt = 'NASA Picture of the Day';
-              image.loading = 'lazy';
-              image.classList.add('card-img-top');
-        // Card Body
-        const cardBody = document.createElement('div');
-              cardBody.classList.add('card-body');
-        // Card Title
-        const cardTitle = document.createElement('h5');
-              cardTitle.classList.add('card-title');
-              cardTitle.textContent = result.title;
-        // Save Text
-        const saveText = document.createElement('p');
-              saveText.classList.add('clickable');
+            currentArray.forEach((result) => {
+                // Card Container
+                const card = document.createElement('div');
+                    card.classList.add('card');
+                // Link 
+                const link = document.createElement('a');
+                    link.href = result.dhUrl;
+                    link.title = 'View Full Image';
+                    link.target = '_blank';
+                // Image
+                const image = document.createElement('img');
+                    image.src = result.url;
+                    image.alt = 'NASA Picture of the Day';
+                    image.loading = 'lazy';
+                    image.classList.add('card-img-top');
+                // Card Body
+                const cardBody = document.createElement('div');
+                    cardBody.classList.add('card-body');
+                // Card Title
+                const cardTitle = document.createElement('h5');
+                    cardTitle.classList.add('card-title');
+                    cardTitle.textContent = result.title;
+                // Save Text
+                const saveText = document.createElement('p');
+                    saveText.classList.add('clickable');
               
               if(page === 'results'){
                     saveText.style.cursor='pointer';
@@ -61,25 +61,25 @@ function createDOMNodes(page){
                     saveText.style.cursor='pointer';
                     saveText.setAttribute('onclick', `removeFavorite('${result.url}')`);
               }
-        // Card Text
-            const cardText = document.createElement('p');
-                cardText.textContent = result.explanation;
-        // Footer Container 
-            const footer = document.createElement('small');
-                footer.classList.add('text-muted');
-        // Date 
-            const date = document.createElement('strong');
-                date.textContent = result.date;
-        // Copyright
-            const copyrightResult = result.copyright === undefined ? '' : result.copyright;
-            const copyRight = document.createElement('span');
-                copyRight.textContent = ` ${copyrightResult}`;
-        // Now append these elements right order
-            footer.append(date, copyRight);
-            cardBody.append(cardTitle, saveText, cardText,footer);
-            link.appendChild(image);
-            card.append(link, cardBody);
-            imagesContainer.appendChild(card);
+                // Card Text
+                    const cardText = document.createElement('p');
+                        cardText.textContent = result.explanation;
+                // Footer Container 
+                    const footer = document.createElement('small');
+                        footer.classList.add('text-muted');
+                // Date 
+                    const date = document.createElement('strong');
+                        date.textContent = result.date;
+                // Copyright
+                    const copyrightResult = result.copyright === undefined ? '' : result.copyright;
+                    const copyRight = document.createElement('span');
+                        copyRight.textContent = ` ${copyrightResult}`;
+                // Now append these elements right order
+                    footer.append(date, copyRight);
+                    cardBody.append(cardTitle, saveText, cardText,footer);
+                    link.appendChild(image);
+                    card.append(link, cardBody);
+                    imagesContainer.appendChild(card);
     });
 }
 
@@ -89,7 +89,6 @@ function updateDOM(page){
         favorites = JSON.parse(localStorage.getItem('nasaFavorites'));
     }
     imagesContainer.textContent = '';
-    
     createDOMNodes(page);   
     showContent(page);
 }
@@ -139,3 +138,4 @@ async function getNasaPictures(){
 
 // On Load
 getNasaPictures(); 
+
